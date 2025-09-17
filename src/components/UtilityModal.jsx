6e53f2fea4ec87c6d20ff76603b8d7b3583cb383
chrisjcoates <chriscoates9@gmail.com>
2025-09-17 17:43:03 +0100

    Update UtilityModal styling to dark theme
    Changed the modal background to gray-800 and added a gray-600 border for a darker appearance.

--- src/components/UtilityModal.jsx
+++ src/components/UtilityModal.jsx
@@ -3,7 +3,7 @@ const UtilityModal = ({ isOpen, onEditClick, onCloseClick, onDeleteClick }) => {
 
   return (
     <div className='fixed inset-0 bg-black/50 z-40'>
-      <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white p-4 shadow-lg rounded-lg w-100 flex flex-row justify-around'>
+      <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-gray-800 p-4 border border-gray-600 shadow-lg rounded-lg w-100 flex flex-row justify-around'>
         <button
           onClick={onEditClick}
           className='py-1 w-full mr-3 bg-green-600 text-white hover:bg-green-700 rounded cursor-pointer'
