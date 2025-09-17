8fcddd3a89df9050a38564dcf38f7e020c5ede61
chrisjcoates <chriscoates9@gmail.com>
2025-09-17 11:01:39 +0100

    Update TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,11 +1,20 @@
-const TaskForm = () => {
+const TaskForm = ({ formData, setFormData }) => {
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className='mb-3'>
         <label htmlFor='task'>Task</label>
         <textarea
           className='w-full p-2 border border-gray-400 rounded-lg'
-          name='task'
+          name='description'
+          value={formData.description}
+          onChange={handleChange}
           rows='5'
           placeholder='Enter task...'
         ></textarea>
@@ -15,7 +24,8 @@ const TaskForm = () => {
         <select
           className='w-full p-2 border border-gray-400 rounded-lg'
           name='priority'
-          id=''
+          value={formData.priority}
+          onChange={handleChange}
         >
           <option value='High'>High</option>
           <option value='Medium'>Medium</option>
